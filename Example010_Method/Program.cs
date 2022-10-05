bbe0f99c04540ed75ce8ec5ed8139a91f274e937
Juliya <9123288799@mail/ru>
2022-10-05 19:30:43 +0500

    Add 010

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, 2, 3, 4, 18, 6, 7, 18, 9};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;//этим действием находим только первый элемент из всех одинаковых
+    }
+    // index = index +1;
+    index++;
+}
